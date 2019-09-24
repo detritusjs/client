695d865725bf2f1978a03d9b364df05fc3d08df8
Dan <dansoul.cake@gmail.com>
2019-09-24 09:51:19 -0400

    client-rest version bump

--- src/constants.ts
+++ src/constants.ts
@@ -58,7 +58,7 @@ export { DISCORD_SNOWFLAKE_EPOCH, DISCORD_TOKEN_EPOCH };
 
 export const Package = Object.freeze({
   URL: 'https://github.com/detritusjs/client',
-  VERSION: '0.7.7',
+  VERSION: '0.7.8',
 });
 
 export type Snowflake = number | string;
