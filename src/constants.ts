80cd8f5d2982ed4dbbc5cde869a9ac0cacb7089c
Dan <31395415+cakedan@users.noreply.github.com>
2021-06-05 13:16:14 -0600

    0.15.1

--- src/constants.ts
+++ src/constants.ts
@@ -35,7 +35,7 @@ export {
 
 export const Package = Object.freeze({
   URL: 'https://github.com/detritusjs/client',
-  VERSION: '0.15.1-beta.22',
+  VERSION: '0.15.1',
 });
 
 export type Snowflake = number | string;
